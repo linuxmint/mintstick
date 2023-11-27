50ab727e112a660e5fe2539bcaf536c8cf1d3c39
Michael Webster <miketwebster@gmail.com>
2023-11-27 09:27:48 -0500

    Fix generate_additional_files script for policykit changes.

--- generate_additional_files.py
+++ generate_additional_files.py
@@ -94,7 +94,7 @@ prefix="""<?xml version="1.0" encoding="UTF-8"?>
   <vendor>Linux Mint</vendor>
   <vendor_url>https://linuxmint.com</vendor_url>
 
-  <action id="com.linuxmint.mintstick">
+  <action id="com.linuxmint.mintstick-write">
     <description>USB Image Writer / USB Stick Formatter</description>
 """
 
@@ -105,14 +105,41 @@ suffix="""
       <allow_inactive>no</allow_inactive>
       <allow_active>auth_admin_keep</allow_active>
     </defaults>
-    <annotate key="org.freedesktop.policykit.exec.path">/usr/bin/python3</annotate>
-    <annotate key="org.freedesktop.policykit.exec.argv1">/usr/lib/mintstick/raw_write.py</annotate>
-    <annotate key="org.freedesktop.policykit.exec.argv1">/usr/lib/mintstick/raw_format.py</annotate>
+    <annotate key="org.freedesktop.policykit.exec.path">/usr/bin/mint-stick-write</annotate>
     <annotate key="org.freedesktop.policykit.exec.allow_gui">true</annotate>
   </action>
 
 </policyconfig>
 """
 
-additionalfiles.generate_polkit_policy(DOMAIN, PATH, "share/polkit/com.linuxmint.mintstick.policy", prefix, _("This will destroy all data on the USB stick, are you sure you want to proceed?"), suffix)
+additionalfiles.generate_polkit_policy(DOMAIN, PATH, "share/polkit/com.linuxmint.mintstick-write.policy", prefix, _("This will destroy all data on the USB stick, are you sure you want to proceed?"), suffix)
+
+prefix="""<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE policyconfig PUBLIC
+ "-//freedesktop//DTD PolicyKit Policy Configuration 1.0//EN"
+ "http://www.freedesktop.org/standards/PolicyKit/1/policyconfig.dtd">
+<policyconfig>
+
+  <vendor>Linux Mint</vendor>
+  <vendor_url>https://linuxmint.com</vendor_url>
+
+  <action id="com.linuxmint.mintstick-format">
+    <description>USB Image Writer / USB Stick Formatter</description>
+"""
+
+suffix="""
+    <icon_name>mintstick</icon_name>
+    <defaults>
+      <allow_any>no</allow_any>
+      <allow_inactive>no</allow_inactive>
+      <allow_active>auth_admin_keep</allow_active>
+    </defaults>
+    <annotate key="org.freedesktop.policykit.exec.path">/usr/bin/mint-stick-format</annotate>
+    <annotate key="org.freedesktop.policykit.exec.allow_gui">true</annotate>
+  </action>
+
+</policyconfig>
+"""
+
+additionalfiles.generate_polkit_policy(DOMAIN, PATH, "share/polkit/com.linuxmint.mintstick-format.policy", prefix, _("This will destroy all data on the USB stick, are you sure you want to proceed?"), suffix)
 
