74e4e9d96a76275d291a4f433d6638311a065c73
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2024-01-17 11:40:14 +0000

    iso: Add syslog debug calls and exception handling

--- lib/raw_write.py
+++ lib/raw_write.py
@@ -5,47 +5,55 @@ import argparse
 sys.path.append('/usr/lib/mintstick')
 from mountutils import do_umount
 import parted
+import syslog
 
 def raw_write(source, target):
-    do_umount(target)
-    bs = 4096
-    size=0
-    input = open(source, 'rb')
-    total_size = float(os.path.getsize(source))
+    syslog.syslog(f"Writing '{source}' on '{target}'")
+    try:
+        do_umount(target)
+        bs = 4096
+        size=0
+        input = open(source, 'rb')
+        total_size = float(os.path.getsize(source))
 
-    # Check if the ISO can fit ... :)
-    device = parted.getDevice(target)
-    device_size = device.getLength() * device.sectorSize
-    if device_size < float(os.path.getsize(source)):
+        # Check if the ISO can fit ... :)
+        device = parted.getDevice(target)
+        device_size = device.getLength() * device.sectorSize
+        if device_size < float(os.path.getsize(source)):
+            input.close()
+            print("nospace")
+            exit(3)
+
+        increment = total_size / 100
+
+        written = 0
+        output = open(target, 'wb')
+        while True:
+            buffer = input.read(bs)
+            if len(buffer) == 0:
+                break
+            output.write(buffer)
+            size = size + len(buffer)
+            written = written + len(buffer)
+            print(size/total_size)
+            if (written >= increment):
+                output.flush()
+                os.fsync(output.fileno())
+                written = 0
+
+        output.flush()
+        os.fsync(output.fileno())
         input.close()
-        print("nospace")
-        exit(3)
-
-    increment = total_size / 100
-
-    written = 0
-    output = open(target, 'wb')
-    while True:
-        buffer = input.read(bs)
-        if len(buffer) == 0:
-            break
-        output.write(buffer)
-        size = size + len(buffer)
-        written = written + len(buffer)
-        print(size/total_size)
-        if (written >= increment):
-            output.flush()
-            os.fsync(output.fileno())
-            written = 0
-
-    output.flush()
-    os.fsync(output.fileno())
-    input.close()
-    output.close()
-    if size == total_size:
-        print("1.0")
-        exit (0)
-    else:
+        output.close()
+        if size == total_size:
+            print("1.0")
+            exit (0)
+        else:
+            print("failed")
+            exit (4)
+    except Exception as e:
+        syslog.syslog("An exception occured")
+        syslog.syslog(str(e))
         print("failed")
         exit (4)
 
