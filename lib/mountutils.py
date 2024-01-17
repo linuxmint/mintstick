568dbfd0356c1f06ab51c53a2c3f567301c5cbba
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2024-01-17 11:39:12 +0000

    mountutils: Debug with syslog rather than print

--- lib/mountutils.py
+++ lib/mountutils.py
@@ -1,25 +1,29 @@
 import sys
 from subprocess import call
+import syslog
 
 def do_umount(target):
         mounts = get_mounted(target)
         if mounts:
-            print('Unmounting all partitions of '+target+':')
+            syslog.syslog(f"Unmounting all partitions of {target}.")
         for mount in mounts:
-            print('Trying to unmount '+mount[0]+'...')
+            device = mount[0]
+            syslog.syslog(f"Trying to unmount {device}...")
             try:
-                retcode = call('umount '+mount[0], shell=True)
+                retcode = call(f"umount {device}", shell=True)
                 if retcode < 0:
-                    print('Error, umount '+mount[0]+' was terminated by signal '+str(retcode))
+                    error = str(retcode)
+                    syslog.syslog(f"Error, umount {device} was terminated by signal {error}")
                     sys.exit(6)
                 else:
                     if retcode == 0:
-                        print(mount[0]+' successfully unmounted')
+                        syslog.syslog(f"{device} successfully unmounted")
                     else:
-                        print('Error, umount '+mount[0]+' returned '+str(retcode))
+                        syslog.syslog(f"Error, umount {device} returned 0")
                         sys.exit(6)
             except OSError as e:
-                print('Execution failed: '+str(e))
+                error = str(e)
+                syslog.syslog(f"Execution failed: {error}")
                 sys.exit(6)
 
 
@@ -28,5 +32,5 @@ def get_mounted(target):
             lines = [line.strip("\n").split(" ") for line in open ("/etc/mtab", "r").readlines()]
             return [mount for mount in lines if mount[0].startswith(target)]
         except:
-             print('Could not read mtab !')
+             syslog.syslog('Could not read mtab!')
              sys.exit(6)
